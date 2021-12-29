078eb30b2172fcd63ee2e2146a6ba92a930be699
p-wojt <patryk.wojtiuk9@gmail.com>
2021-12-29 20:53:47 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/app.ts',
+    output: {
+        path:path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: 'dist'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, "./"),
+        },
+    },
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: './src/app.ts',
+    output: {
+        path:path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: 'dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, "./"),
+        },
+    },
+};
